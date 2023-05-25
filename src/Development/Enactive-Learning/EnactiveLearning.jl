bf31e3f7c1d4b2da3d5d38b14d8d0d6291dc3abb
Hans-2020 <93684177+VOXDelta@users.noreply.github.com>
2023-05-26 00:46:34 +0200

    Up

---
+++ src/Development/Enactive-Learning/EnactiveLearning.jl
@@ -0,0 +1,14 @@
+"""Description of the whole project"""
+module EnactiveLearning
+
+include("./AgentTools.jl")
+include("./components/world.jl")
+
+
+using Agents, GLMakie, InteractiveDynamics, Random
+
+
+
+
+
+end # module EnactiveLearning
---
+++ src/Development/Enactive-Learning/components/atom.jl
@@ -0,0 +1,9 @@
+
+
+@agent Atom ContinuousAgent{2} begin
+secretion_rate::Float64
+absorbtion_rate::Float64
+influenc_range::Int64
+speed::Float64 #alternativ Vector with (x,y)
+
+end
