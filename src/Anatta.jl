1e5dd06cb5f5f85ccba6354da9baa94c14445dcf
Niall Palfreyman <Niall.Palfreyman@hswt.de>
2023-09-03 17:25:35 +0200

    Update Anatta.jl

--- src/Anatta.jl
+++ src/Anatta.jl
@@ -7,6 +7,9 @@ Anatta: Understanding Nature in terms of processes instead of things.
 This is the central switchboard for the Anatta project, which will gradually build up into a
 full course in using Julia to understand the world in terms of non-self (anatta).
 
+To get started, simply install and load the Anatta library, then enter:
+Anatta.go()
+
 Author: Niall Palfreyman, 01/01/2023
 """
 module Anatta
