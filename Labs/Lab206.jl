e2cdf44cfc9a67488d7a6b7497a85bd15e428f07
Niall Palfreyman <Niall.Palfreyman@hswt.de>
2024-04-07 19:17:45 +0200

    Updated labs 206-8

---
+++ Labs/Lab206.jl
@@ -0,0 +1,18 @@
+#========================================================================================#
+#	Laboratory 206
+#
+# Metastability
+#
+# Author: Niall Palfreyman, 04/04/2024
+#========================================================================================#
+[
+    Activity(
+        """
+        Hi! Welcome to Anatta Lab 206: Switching in multi-stable systems!
+
+        Please work through the script file Script206.pdf.
+        """,
+        "",
+        x -> true
+    ),
+]
---
+++ Labs/Lab208.jl
@@ -0,0 +1,18 @@
+#========================================================================================#
+#	Laboratory 208
+#
+# Bifurcation
+#
+# Author: Niall Palfreyman, 04/04/2024
+#========================================================================================#
+[
+    Activity(
+        """
+        Hi! Welcome to Anatta Lab 208: Bifurcations and catastrophes!
+
+        Please work through the script file Script208.pdf, referring to the file Bifurcations.jl.
+        """,
+        "",
+        x -> true
+    ),
+]
