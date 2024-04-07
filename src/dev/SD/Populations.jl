0d98a5ffc2b36b59d5a910646f091ab599acbdae
Niall Palfreyman <Niall.Palfreyman@hswt.de>
2024-04-07 19:18:17 +0200

    Update Populations.jl
    Implemented Holling-Tanner bifurcation

--- src/dev/SD/Populations.jl
+++ src/dev/SD/Populations.jl
@@ -9,7 +9,7 @@ Author: Niall Palfreyman, 18/03/2024
 """
 module Populations
 
-using DynamicalSystems, GLMakie
+using DynamicalSystems, GLMakie, Observables
 
 #-----------------------------------------------------------------------------------------
 # Module types:
@@ -29,9 +29,9 @@ const model = [
 		variables	= ["x"],
 		initial		= [0.01],						# Population x
 		parameters	= [0.2,50],						# r, K
-		duration	= 20,
+		duration	= 100,
 		flow		= function (du,u,p,t)
-			du[1] = p[1] * u[1] * (1-u[1]/p[2])		# Logistic growth equation
+			du[1] = p[1] * u[1] * (1 - u[1]/p[2])	# Logistic growth equation
 			nothing
 		end
 	),
@@ -55,12 +55,16 @@ const model = [
 	),
 	(
 		title		= "Holling-Tanner",
-		variables	= ["x"],
-		initial		= [0.01],						# Population x
-		parameters	= [0.2,50],						# alpha, K
-		duration	= 20,
+		variables	= ["x", "y", "N"],
+		initial		= [0.1, 0.1, 2.5],				# Population x, y and N
+		parameters	= [1.0,0.2,7.0,6/7,1.0],		# r1, r2, k, w, d
+		duration	= 50,
 		flow		= function (du,u,p,t)
-			du[1] = p[1] * u[1] * (1-u[1]/p[2])		# Logistic growth equation
+			prey_growth = p[1]*u[1]*(1 - u[1]/p[3])
+			predator_growth = p[2]*u[2]*(1 - u[3]*u[2]/u[1])
+			saturated_predation = p[4]*u[1]*u[2]/(p[5] + u[1])
+			du[1] = prey_growth - saturated_predation
+			du[2] = predator_growth
 			nothing
 		end
 	)
@@ -70,9 +74,9 @@ const model = [
 # Module methods:
 #-----------------------------------------------------------------------------------------
 """
-	demo()
+	demo( m)
 
-Demonstrate how to run the model.
+Demonstrate how to run the m-th model.
 """
 function demo( m::Int=1)
 	u,t = trajectory(
@@ -89,6 +93,41 @@ function demo( m::Int=1)
 	end
 	Legend( fig[1,2], ax)
 	display(fig)
+
+	nothing
+end
+
+#-----------------------------------------------------------------------------------------
+"""
+	holling()
+
+Traverse the Holling-Tanner bifurcation in N.
+"""
+function holling()
+	ht = model[3]
+	fig = Figure(fontsize=30,linewidth=5)
+	ax = Axis(fig[1,1], xlabel="x", ylabel="y", title=ht.title)
+	limits!( ax, 0, 7, 0, 6)
+
+	# Set up and run animation:
+	for N in 2.5:-0.05:0.5
+		empty!(ax)
+		for x in [0.5,7.0], y in 0.5:1.0:5.5
+			u,_ = trajectory( CoupledODEs(ht.flow,[x,y,N],ht.parameters), 50, Δt = 0.1)
+			lines!( u[:,1], u[:,2])
+		end
+		for x in 1.0:1.0:7.0, y in [0.5,5.5]
+			u,_ = trajectory( CoupledODEs(ht.flow,[x,y,N],ht.parameters), 50, Δt = 0.1)
+			lines!( u[:,1], u[:,2])
+		end
+		for x in 0.5:0.5:2.0, y in 2.0:0.5:3.0
+			u,_ = trajectory( CoupledODEs(ht.flow,[x,y,N],ht.parameters), 50, Δt = 0.1)
+			lines!( u[:,1], u[:,2])
+		end
+		text!( string( "N = ", N), position=(5.0,5.0), fontsize=30, align=(:left,:center))
+		display(fig)
+		sleep(0.1)
+	end
 
 	nothing
 end
