6344b9dc8a6e5cebd30038ee829d06e0a0295768
Niall Palfreyman <Niall.Palfreyman@hswt.de>
2024-04-12 11:56:08 +0200

    Update Pendulum.jl
    Set up Pendulum.demo() to perform simple damped motion, building up through commented changes to full Poincare section of chaotic motion.

--- src/dev/SD/Pendulum.jl
+++ src/dev/SD/Pendulum.jl
@@ -26,9 +26,11 @@ const model = (
 	title		= "Archie",
 	variables	= ["theta","theta_dot"],
 	initial		= [0.8,0.8],					# theta, theta_dot
+	parameters	= [0.0,0.2,0.694],				# alpha, beta, omega
 #	parameters	= [0.52,0.2,0.694],				# alpha, beta, omega
-	parameters	= [0.6,0.1,0.694],				# alpha, beta, omega
-	duration	= 500,
+#	parameters	= [0.6,0.1,0.694],				# alpha, beta, omega
+	duration	= 30,
+#	duration	= 500,
 	flow		= function (du,u,p,t)
 		du[1]	= u[2]
 		du[2]	= p[1]*cos(p[3]*t) - p[2]*u[2] - sin(u[1])
@@ -56,22 +58,23 @@ Demonstrate how to run the model.
 """
 function demo()
 	du = CoupledODEs(model.flow,model.initial,model.parameters)
-	u,t = trajectory( du, model.duration, Δt = 0.1, Ttr = 100)
+	u,t = trajectory( du, model.duration, Δt = 0.1)
+#	u,t = trajectory( du, model.duration, Δt = 0.1, Ttr = 100)
 
-	fig = Figure(fontsize=30,linewidth=5,title=model.title)
+	fig = Figure(fontsize=30,linewidth=3,title=model.title)
 	ax = Axis(fig[1,1], title="BOTG", xlabel="time")
-	lines!( t, orient.(u[:,1]), label=model.variables[1])
+	lines!( t, u[:,1], label=model.variables[1])
+#	lines!( t, orient.(u[:,1]), label=model.variables[1])
 	lines!( t, u[:,2], label=model.variables[2])
 
-	Axis(fig[2, 1], title="Phase plot",
-		xlabel=model.variables[1], ylabel=model.variables[2],
-		aspect=1
+	Axis(fig[2, 1], title="Phase plot", aspect=1,
+		xlabel=model.variables[1], ylabel=model.variables[2]
 	)
-#	scatterlines!( u[:,1], u[:,2])
-#	scatterlines!( orient.(u[:,1]), u[:,2])
+	lines!( u[:,1], u[:,2])
+#	lines!( orient.(u[:,1]), u[:,2])
 #	psect,_ = trajectory( du, model.duration, Δt = 9.0535735, Ttr = 100)
-	psect,_ = trajectory( du, 100_000, Δt = 9.0535735, Ttr = 100)
-	scatter!(orient.(psect[:,1]),psect[:,2],markersize=5)
+#	psect,_ = trajectory( du, 100_000, Δt = 9.0535735, Ttr = 100)
+#	scatter!(orient.(psect[:,1]),psect[:,2],markersize=5)
 
 	Legend( fig[1,2], ax)
 	display(fig)
