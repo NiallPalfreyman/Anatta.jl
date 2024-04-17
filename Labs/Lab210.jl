faba6d50cb6f78b40484603abe9cae5d3c32a502
Niall Palfreyman <Niall.Palfreyman@hswt.de>
2024-04-17 13:02:10 +0200

    Update Lab210.jl
    Adapted text to Rapa-nui ecosystem.

--- Labs/Lab210.jl
+++ Labs/Lab210.jl
@@ -1,16 +1,16 @@
 #========================================================================================#
 #	Laboratory 210
 #
-# Modelling ecological systems
+# How to build a dynamical model
 #
 # Author: Niall Palfreyman, 11/04/2024
 #========================================================================================#
 [
     Activity(
         """
-        Hi! Welcome to Anatta Lab 210: Modelling an ecological system
+        Hi! Welcome to Anatta Lab 210: How to build a dynamical model
 
-        Please work through the script file Script210.pdf, referring to the file Kaibab.jl.
+        Please work through the script file Script210.pdf, referring to the file RapaNui.jl.
         """,
         "",
         x -> true
