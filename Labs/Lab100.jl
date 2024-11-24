29ee32c63909ab2ecddbd6172a72c937664ced94
Niall Palfreyman <Niall.Palfreyman@hswt.de>
2024-11-24 19:50:49 +0100

    Update Lab100.jl
    Inserted spaces in the BNF definition

--- Labs/Lab100.jl
+++ Labs/Lab100.jl
@@ -91,7 +91,7 @@ const parse_tests = [
             variable    : letter * number.
             constant    : "T"; "F".
             unary_wff   : "~" * wff.
-            binary_wff  : "("*wff * "&" * wff*")"; "("*wff * "|" *wff*")"; "("*wff * "->" * wff*")".
+            binary_wff  : "("*wff * " & " * wff*")"; "("*wff * " | " *wff*")"; "("*wff * " -> " * wff*")".
             letter      : "a"; "b"; "c"; ...; "x"; "y"; "z".
             number      : digit; digit * number.
             digit       : "0"; "1"; "2"; "3"; "4"; "5"; "6"; "7"; "8"; "9".
