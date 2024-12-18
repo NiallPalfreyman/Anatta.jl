df699859a4a912caa5b0e77dadb7903aabb0e103
Niall Palfreyman <Niall.Palfreyman@hswt.de>
2024-12-18 21:54:11 +0100

    Update Roulette.jl
    Improved documentation of sample().

--- src/dev/Computation/Roulette.jl
+++ src/dev/Computation/Roulette.jl
@@ -13,43 +13,45 @@ using Statistics: mean, std
 # Module methods:
 #-----------------------------------------------------------------------------------------
 """
-	sample( frequencies::Vector{Float64}, n=length(freq), T=0.0)
+	sample( x::Vector{Float64}, n=length(x), T=0.0)
 
-Create a vector of n sampled types from a population containing types with the given current
-frequencies. This sampling is achieved using roulette-wheel selection of the types based on
-a roulette wheel whose pockets are biased according to the frequencies. T is a "Temperature"
-parameter: when it is high (1.0), the sampling is more random; when it is low (0.0), the
-sampling is increasingly defined by the current distribution of frequencies. Use sigma-scaling
-to eliminate all types from sampling whose frequency is less than 1/(2T+0.1) standard deviations
-below the frequency mean. 
+Create a vector of n sampled types from a population whose types have population frequencies
+given by the vector x. Sampling is implemented using roulette-wheel selection based on a
+roulette wheel whose pockets are biased according to the frequencies.
+
+T is a "Temperature" parameter that determines the extent to which sampling depends upon the
+frequency distribution. When T is high (T>=1.0), the sampling is (rigidly) uniformly distributed;
+when T is low (T<=0.0), sampling becomes increasingly determined by the frequency distribution
+vector x. This implementation also uses sigma-scaling to eliminate from sampling all types whose
+frequency is smaller than 1/(2T+0.1) standard deviations below the frequency mean.
 """
-function sample( frequencies::Vector{Float64}, n=length(frequencies); T=0.0)
+function sample( x::Vector{Float64}, n=length(x); T=0.0)
 	# Preprocess frequencies:
 	T = min(1.0,max(0.0,T))								# Enforce 0.0 <= T <= 1.0
-	frequencies = T*mean(frequencies) .+ (1-T)*frequencies	# Smooth frequencies towards average
-	frequencies = frequencies / sum(frequencies)		# Normaise the resulting frequencies
-	avg = mean(frequencies)								# Average population frequency
-	stdev = std(frequencies)							# Spread of frequencies from average
+	x = T*mean(x) .+ (1-T)*x							# Smooth frequencies towards average
+	x = x / sum(x)										# Normaise the resulting frequencies
+	avg = mean(x)										# Average population frequency
+	stdev = std(x)										# Spread of frequencies from average
 
 	# Create fitness values:
 	if stdev == 0										# Singular case: All frequencies were
-		fitnesses = ones(Float64,length(frequencies))	# equal to the average!
+		fitnesses = ones(Float64,length(x))				# equal to the average!
 	else
-		fitnesses = 1.0 .+ (frequencies.-avg)/((0.1+2T)*stdev)
-		fitnesses[fitnesses .<= 0] .= 0.0				# Frequencies smaller than 1/2T std
-	end													# deviations below average are unviable
+		fitnesses = 1.0 .+ (x.-avg)/((0.1+2T)*stdev)	# Frequencies smaller than 1/(2T+0.1) std
+		fitnesses[fitnesses .<= 0] .= 0.0				# deviations below average are unviable
+	end
 	
 	# Create fitness-biased roulette wheel:
 	wheel = cumsum( fitnesses)/sum(fitnesses)			# Roulette-wheel of fitness-biased pockets
-	throws = (rand() .+ (1:n)/n) .% 1.0					# nsamples uniformly distributed ball-throws
-
-	# Create vector of sampled freqency types:
+	throws = (rand() .+ (1:n)/n) .% 1.0					# Stochastic Universal Sampling: n ball-throws
+														# distributed uniformly over interval (0,1)
+	# Create vector of n sampled population types:
 	samples = zeros(Integer,n)
 	for (throw,sample) in enumerate(throws)				# Ball-throw delivers one random sample fitness
 		for (pocket,fitness) in enumerate(wheel)		# Wheel pocket contains fitness of one type
 			if sample <= fitness						# When the sample surpasses type's fitness ...
 				samples[throw] = pocket					# ... use this pocket as the sampled type
-				break									# ... then break out to the next throw
+				break									# ... then skip on to the next throw.
 			end
 		end
 	end
