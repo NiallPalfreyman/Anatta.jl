2ef595a132857d8929cd0b41e763216890dabb9d
Niall Palfreyman <Niall.Palfreyman@hswt.de>
2025-03-09 20:23:44 +0100

    Completed lab 606

--- Labs/Lab606.jl
+++ Labs/Lab606.jl
@@ -1,107 +1,192 @@
 #========================================================================================#
 #	Laboratory 606
 #
-# Neutral drift.
+# Flow dynamics and waves.
 #
-# Author: Niall Palfreyman (March 2023) and Nick Diercksen (July 2022)
+# Author: Niall Palfreyman, March 2025
 #========================================================================================#
 [
     Activity(
         """
-        Welcome to Lab 606: Neutral Drift.
+        Hi! Welcome to Anatta Subject 606:
+            Fields as independent dynamical entities.
 
-        Neutral drift is the tendency of genetic structure to mutate randomly, even in conditions
-        where there is no strong selection pressure from the environment.
+        Fields are not only produced and consumed by agents; they also possess their own dynamics,
+        which can be highly complex - as we shall see in this lab. In fact, ALL biological systems
+        contain two essential features: An agent structure that generates and utilises fields
+        LOCALLY, and a set of dynamical fields that interact NONLOCALLY, which and influence
+        variations in the agent structures. These structures may be organisms, cells, neurons or
+        genomes, and the fields may be food resources, messenger proteins, or transcription factors.
 
-        For example, suppose that a particular tribe happens to move to an iron-rich environment
-        which makes them grow taller, giving them the selective advantage of being able to reach
-        higher-hanging fruit. Of course, if the tribe moves away to an iron-poor environment,
-        individuals will no longer have this advantage. However, while they live in the iron-rich
-        environment, neutral drift will continue, and competition between individuals will tend to
-        favour those individuals that are RELIABLY taller. Consequently, over generations, other
-        phenotypic traits will be selected that maintain tallness even when individuals absorb less
-        iron from their environment:
-            Neutral drift can genetically fix a trait that is induced by the environment!
+        In this lab we discover that fields are not just passive media that convey signals between
+        agents, but are active entities in their own right that generate and transform themselves.
+        We will model the nonlocal field dynamics of the waves underlying quantum particles and
+        light photons, and see how these fields can generate complex interference and diffraction
+        patterns. We will also use our agent-based model to visualise the complex phenomenon of
+        interference - this pedagogical application is important in computer-aided instruction.
+        """,
+        "",
+        x -> occursin("interfer",lowercase(x))
+    ),
+    Activity(
+        """
+        Load the module Maxwell, which implements a particular solution of Maxwells Equations.
+        
+        From 1862-1865, James Clerk Maxwell formulated his four equations to describe how electric
+        and magnetic fields (E-fields and B-fields) are related to each other and to the wave
+        phenomenon of light. In particular, Maxwell's equations demonstrate that fields possess an
+        existence of their own - quite independent of the charged particles that generate them. The
+        equations possess a symmetry that allows the fields to generate each other in a self-
+        sustaining manner, forming waves that propagate themselves across the Universe without any
+        material structures to support them.
+        
+        The central evidence for the wavelike nature of light is the effect simulated in the Maxwell
+        module. Study the simulation Maxwell.demo() now, and tell me the correct name for the
+        interaction that you observe between the two sets of waves.
+        """,
+        "Use the mouse cursor to find locations where the waves from the two synchronised sources " *
+            "cancel each other out",
+        x -> occursin("interfer",lowercase(x))
+    ),
+    Activity(
+        """
+        I have used the parameter "attenuation" to ensure that the wave pattern is not complicated
+        by waves that flow over the edge of the model's world. Adjust the value of attenuation to
+        zero, and observe the effect on the wave pattern. At the edge of the graphics
+        representation, you can see that the waves wrap around to the other side of the world. This
+        is a common feature of agent-based models, known as "periodic boundary conditions". Here,
+        the waves look as though they are displaying a common wave phenomenon called ... what?
+        """,
+        "The edges look as if they are acting like mirrors",
+        x -> occursin("refle",lowercase(x)) || occursin("bounc",lowercase(x))
+    ),
+    Activity(
+        """
+        Change the value of attenuation to its maximum allowed value, and observe the effect on the
+        wave pattern. The waves now die out very quickly. You can use the 'sleep' parameter to slow
+        down the simulation and see what is happening in more detail.
 
-        Neutral drift of genetic structure therefore plays a hugely important role in evolution. If
-        evolutionary change is a ratchet that moves towards increased fitness, neutral drift is the
-        pawl that prevents this ratchet from going back to previous, lower-fitness states. Neutral
-        drift is the ability of evolutionary systems to cement random variations into a CHOICE.
-
-        Let's look at this now. Run the module NeutralDrift several times and observe its dynamics.
+        Next, restore attenuation to its original value of 0.4 so that we can study the
+        interference pattern of the waves. The interference pattern is a result of the
+        superposition of the two waves:
+        -   At points where the peaks of the two waves coincide, we say that the waves are In Phase
+            with each other, and they add to form waves of maximum combined amplitude;
+        -   At points where the peak of one wave coincides with the trough of the other, we say
+            that the waves are In Anti-phase, and they cancel each other out to form waves of
+            minimum amplitude.
         """,
         "",
         x -> true
     ),
     Activity(
         """
-        What you have observed is called a Moran Process: many agents exchange individual decisions
-        until they collectively make an irreversible choice. We can interpret this Moran process in
-        three different ways - evolutionarily, developmentally or socially:
-            -	EVOLUTIONARILY, altruism is genetically determined. Agents have a random genetic
-                tendency to act altruistically. In each generation, agents die and are replaced by
-                a random nearby individual. They therefore tend to be replaced by the most frequent
-                phenotype in their reproductive neighbourhood.
-            -	DEVELOPMENTALLY, altruism is physiologically determined. Cells within an organism
-                have a random tendency to support altruistic behaviours of the organism. During
-                development, cells communicate and modify each others' developmental state. They
-                therefore tend to adopt the most common developmental fate in their neighbourhood.
-            -	SOCIALLY, altruism is culturally determined. Agents have a random cultural tendency
-                to act altruistically. At each moment, agents adopt opinions and behaviours from
-                nearby individuals. Over time, they therefore tend to adopt the most common
-                behaviour in their social neighbourhood.
+        Notice how the interference maxima and minima form ray-like paths stretching outwards to
+        right and left from the sources at varying angles. If we imagine the right-hand edge of
+        the world as a projection screen, these paths would show up as a pattern of light (maximum)
+        and dark (minimum) spots along that screen. This is the basis of Young's famous double-slit
+        experiment, which demonstrates that light has an intrinsically wavelike nature.
 
-        Run NeutralDrift again and estimate how frequently the population makes the decision for
-        altruism.
+        If we think of the two sources as two bases in a DNA molecule, each of which independently
+        reflects outwards a single incoming x-ray wave, then you can see how the interference
+        pattern might provide us with information about the distance between those bases, and help
+        us explore the internal structure of the DNA molecule. This exactly what Rosalind Franklin
+        did in 1952, when she used x-ray diffraction to discover the double-helix structure of DNA.
+        Let's use our simulation to explore how x-ray crystallography works ...
         """,
         "",
         x -> true
     ),
     Activity(
         """
-        Notice that this decision is NOT deterministic - it can go either way, and the system can
-        take quite a while to make the decision.
-
-        Now run NeutralDrift again, but this time raise the cost of altruism. How much cost do you
-        need in order to make the population choose consistently against altruistic behaviour?
+        Notice that one ray-path of waves with maximum amplitude stretches horizontally to left and
+        right from a point halfway between the two sources. Light travelling from each of the two
+        sources to points along this path travel equal distances and so will be in phase with each
+        other, and so will constructively interfere to form a bright spot on the screen. We call
+        this spot the Zeroth-Order Maximum of the interference pattern, and is the brightest spot
+        on the screen.
+        
+        The 1st-order maxima are the next brightest spots, appearing just above and below the
+        zeroth-order maximum, where the path-lengths differ by exactly one x-ray wavelength λ. The
+        angle θ between the zeroth- and 1st-order maxima is called the Angle of Diffraction; it is
+        related to the wavelength λ and the distance d between the sources by Bragg's formula:
+            sin(θ) = λ/d ,
+        
+        Test Bragg's formula by varying the value of d while observing the corresponding angle θ.
+        As you use the slide to increase the value of d, does θ increase or decrease?
         """,
         "",
-        x -> (3e-5 ≤ x ≤ 5e-5)
+        x -> occursin("decr",lowercase(x))
     ),
     Activity(
         """
-        As you can see, this Moran choosing process is very unstable! Even a cost of just 0.00003
-        is sufficient to definitely bias the choice. Nevertheless, this choosing process has all
-        the components of biological choice: it is made by the entire system (not just individual
-        components), it is not deterministic, but it is also not pure chance. The choice emerges
-        from the actions of the entire system.
+        Now that we have seen how waves and interference are essential features of light, let's
+        see how these waves arise from their electric and magnetic field components. The electric
+        field (E) is generated by the charge of the sources, and the magnetic field (B) is
+        generated by the changing electric field. The two field strengths E and B are related by
+        the speed of light c, which is the speed at which the waves propagate through space.
+        
+        The method Maxwell.model_step!() simulates this wave behaviour using Euler's method with
+        the dynamical equations for the two phase variables E and B. It develops a numerical
+        solution of the Wave Equation:
+            ∂^2E/∂t^2 = c^2 * ∂^2(E)/∂x^2
 
-        In the social sciences, this model is known as the SCHELLING model, demonstrating how
-        populations choose to adopt new technologies and ideas. In this interpretation, altruism is
-        an opinion that either spreads through a society or else dies out.
+        The wave equation describes two different phenomena: the spreading of fields through space
+        and the wavelike excitation of those fields. The spatial derivatives on the RHS of the
+        equation describe the spreading of fields, while the time derivatives on the LHS describe
+        the wavelike excitation of fields. To explore these two phenomena separately, comment out
+        line 107 of Maxwell.jl, and replace this line of code by:
+            maxi.E[:] = maxi.B[:]
+
+        What is the effect of this code change on the two matrices maxi.E and maxi.B?
+        """,
+        "Create a small matrix A in the REPL, then look at the result of the expression A[:]",
+        x -> occursin("cop",lowercase(x)) && occursin('E',x) && occursin('B',x)
+    ),
+    Activity(
+        """
+        By commenting out line 107 and replacing it with the new code, we have effectively
+        converted the wave equation into the diffusion equation that we met in the previous lab:
+            ∂E/∂t = c^2 * ∂^2(E)/∂x^2
+
+        This diffusion equation specifies the rate of change of E in terms of its spatial
+        curvature: regions of locally high E will diffuse outwards to regions of lower E. You can
+        see that the method Maxwell.model_step!() calculates this curvature. Run Maxwell.demo()
+        now, and observe how the changing E-field at the sources diffuses outwards without any
+        oscillatory motion. This is similar to the behaviour of a front of varying cold or hot
+        temperature that spreads outwards from an extreme source to moderate surrounding regions.
+
+        Now think about this diffusion equation: If the local curvature ∂^2(E)/∂x^2 is very small,
+        how large will the (first order) rate of change ∂E/∂t be? Or in other words:
+            As the field E approaches a uniform temperature, will it move towards that uniform
+            temperature quickly or slowly?
+        """,
+        "What is the relation between the spatial curvature of E and the rate of change of E?",
+        x -> occursin("slow",lowercase(x))
+    ),
+    Activity(
+        """
+        What you have just found is a general property of first-order differential equations: as
+        E approaches a uniform temperature, it changes reeeally slowly, because its spatial
+        curvature specifies the first-order rate of change ∂E/∂t. For this reason, first-order
+        differential equations can never produce oscillatory field activity, but only the slow,
+        smooth spreading of fields.
+
+        Now restore line 107 of Maxwell.jl to its original form and look at the structure of this
+        new line of code. Earlier in Maxwell.model_step!(), the spatial curvature of E specifies
+        the first-order derivative ∂B/∂t of B, so again, B will fall only slowly towards a uniform
+        value. However, the value of B now no longer specifies E itself, but instead E's rate of
+        change ∂E/∂t. In other words, the spatial curvature of E specifies the (second-order)
+        Acceleration of E. This leaves E the freedom to overshoot and 'swing back' to its uniform
+        value, so generating the oscillatory motion described by the wave equation:
+            ∂^2E/∂t^2 = c^2 * ∂^2(E)/∂x^2
+
+        By modifying the method Maxwell.maxwell(), you can add various sources to the model at
+        different positions and with different frequencies and amplitudes. Do this, then run
+        Maxwell.demo() again at various sleep speeds to convince yourself of this essential
+        difference between the diffusion and wave equations: the wave equation allows for
+        oscillatory motion, while the diffusion equation does not.
         """,
         "",
         x -> true
-    ),
-    Activity(
-        """
-        Now perform this experiment: Can you turn the NeutralDrift model into an Ising model? The
-        Ising Model is a simulation of ferromagnetic materials used in statistical mechanics; it
-        also forms the basis for Hopfield neural networks. In the Ising model, the Moran agents no
-        longer interact directly with each other, but instead they interact with a diffusing and
-        evaporating field B. Altruists secrete the signalling factor B, but non-altruists do not.
-        In each step of the model, agents switch their altruist state between true and false by
-        drawing a random value within the range [0,1). If this value is LESS than the local
-        concentration of B, the agent sets its altruist state to TRUE. If the random value is NOT
-        less than the local B concentration, the agent switches its altruist state to FALSE.
-
-        You will find that the behaviour of your Ising model depends crucially upon how fast and
-        far the signalling factor B diffuses outwards from its source, so make sure you create
-        sliders for the secretion, diffusion and evaporation rates of B.
-
-        What role does the field B play in the switching behaviour of Ising Model agents?
-        """,
-        "",
-        x -> occursin("probability",lowercase(x))
     ),
 ]
---
+++ src/dev/Generative/Maxwell.jl
@@ -0,0 +1,147 @@
+#========================================================================================#
+"""
+	Maxwell
+
+This module encapsulates the simulation of an electromagnetic field defined by Maxwell's equations.
+It is capable of simulating the propagation of electromagnetic waves in a 2D space.
+
+Author: Niall Palfreyman, March 2025
+"""
+module Maxwell
+
+include( "../../Development/Generative/AgentTools.jl")
+
+using Agents, GLMakie, .AgentTools
+
+#-----------------------------------------------------------------------------------------
+# Module types:
+#-----------------------------------------------------------------------------------------
+"""
+	Source
+
+An electrical field source (possibly including a time-varying component).
+"""
+@agent struct Source(ContinuousAgent{2,Float64})
+	q0::Float64			# Initial charge
+	f::Float64			# Frequency of oscillation
+	phi::Float64		# Phase of oscillation
+	q::Float64			# Current charge of the source
+end
+
+#-----------------------------------------------------------------------------------------
+# Module methods:
+#-----------------------------------------------------------------------------------------
+"""
+	maxwell( kwargs)
+
+Initialise Maxwell model initially empty of Sources.
+"""
+function maxwell(;
+	d			= 10.0,			# Spacing between sources
+	attenuation = 0.4,			# Attenuation factor for wave propagation (0.0 = no attenuation)
+)
+	extent = (100,100)			# Extent of model space
+	world = ContinuousSpace(extent, spacing=1.0)
+	properties = Dict(
+		:E => zeros(Float64,extent),
+		:B => zeros(Float64,extent),
+		:c => 1.0,
+		:d => d,
+		:t => 0.0,
+		:dt => 0.004,
+		:dxy => 0.1,
+		:attenuation => attenuation,
+	)
+
+	maxi = StandardABM( Source, world; agent_step!, model_step!, properties)
+	halfway = minimum(extent)÷2
+	addsource!( maxi, (halfway,halfway-d/2))
+	addsource!( maxi, (halfway,halfway+d/2))
+
+	maxi
+end
+
+#-----------------------------------------------------------------------------------------
+"""
+	addsource!( maxi, pos, amplitude, frequency, phase)
+
+Add a Source to the Maxwell model with the given specification, initialised to time zero.
+"""
+function addsource!( maxi, pos; vel=(0.0,0.0), q0=1.0, f=1.0, phi=0.0)
+	add_agent!( pos, maxi, vel, q0, f, phi, q0*sin(phi))
+end
+
+#-----------------------------------------------------------------------------------------
+"""
+	agent_step!( src, maxi)
+
+Source agent updates local E-field and moves to new position. Local E-field updates are then
+propagated outwards by the wave equation implemented in model_step!.
+"""
+function agent_step!( src::Source, maxi::ABM)
+	maxi.E[get_spatial_index( src.pos, maxi.E, maxi)] = src.q =
+		src.q0 * sin(src.phi + 2pi*src.f*maxi.t)
+	move_agent!( src, maxi, maxi.dt)
+end
+
+#-----------------------------------------------------------------------------------------
+"""
+	model_step!(maxi)
+
+Approximate a wave solution of Maxwell's equations in 2D space by updating the B-field according
+to the Laplacian of the E-field, and then updating the E-field according to the B-field, where
+the Laplacian operator is approximated by the 2-D curvature of the E-field:
+	d2E/dt2 = c^2 * del^2(E)
+"""
+function model_step!( maxi::ABM)
+	# First calculate first-order Euler change in B due to current E configuration ...
+	maxi.B += maxi.dt * maxi.c^2 * (
+		0.25(
+			circshift(maxi.E,( 1,0)) + circshift(maxi.E,(0, 1)) +
+			circshift(maxi.E,(-1,0)) + circshift(maxi.E,(0,-1))
+		) - maxi.E
+	) / maxi.dxy^2
+	maxi.B *= (1-maxi.attenuation*maxi.dt)			# Attenuate the wave
+
+	# ... then calculate second-order change in E due to current B configuration:
+	maxi.E += maxi.dt * maxi.B
+#	maxi.E[:] = maxi.B[:]
+	maxi.t += maxi.dt
+end
+
+#-----------------------------------------------------------------------------------------
+"""
+	charge( src::Source)
+
+Adapt colour of the source agent according to its charge.
+"""
+charge( src::Source) = (src.q >= 0 ? :yellow : :black)
+
+#-----------------------------------------------------------------------------------------
+"""
+	demo()
+
+Create playground with two wave sources.
+"""
+function demo()
+	params = Dict(
+		:attenuation	=> 0:0.1:5,
+		:d				=> 4:20.0,
+	)
+	plotkwargs = (
+		agent_color = charge,
+		agent_size = 20,
+		heatarray = :E,
+		add_colorbar = false,
+		heatkwargs = (
+			colorrange = (-1,1),
+			colormap = cgrad(:oslo),
+		)
+	)
+
+	playground, = abmplayground( maxwell; params, plotkwargs...)
+
+	playground
+end
+
+end
